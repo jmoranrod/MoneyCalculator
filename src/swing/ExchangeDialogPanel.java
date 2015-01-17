dd9cbc7c570c71cf9ef2547e8c98b827e6c1f293
Javivi <Javivi@Javivi-ASUS>
2015-01-17 13:18:55 +0000

    ExchangeDialogPanel created

--- src/swing/ExchangeDialogPanel.java
+++ src/swing/ExchangeDialogPanel.java
@@ -1,11 +1,25 @@
 package swing;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import model.Currency;
+import model.CurrencySet;
 import model.Exchange;
 import ui.ExchangeDialog;
 
 public class ExchangeDialogPanel extends JPanel implements ExchangeDialog {
+    private CurrencySet currencySet;
+    private JTextField amount;
+    private JComboBox<Currency> inputCurrency;
+    private JComboBox<Currency> outputCurrency;
 
+    public ExchangeDialogPanel(CurrencySet currencySet) {
+        this.currencySet = currencySet;
+    }
+    
+    
+    
     @Override
     public Exchange getExchange() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
