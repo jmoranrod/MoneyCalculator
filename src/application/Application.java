ba9b65443057b04deeeddb22f066de036e4837f9
Javivi <Javivi@Javivi-ASUS>
2015-01-18 19:40:59 +0000

    SQLite implemented.

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,20 +3,24 @@ package application;
 import control.ExchangeOperation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import mock.CurrencySetLoader;
+import java.sql.SQLException;
 import model.CurrencySet;
+import sqlite.CurrencySetLoader;
 import swing.MoneyCalculatorFrame;
 
 public class Application {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         final CurrencySet currencySet = new CurrencySetLoader().load();
         final MoneyCalculatorFrame frame = new MoneyCalculatorFrame(currencySet);
         frame.register("Calculate", new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                new ExchangeOperation(frame.getMoneyDisplayLabel(), frame.getExchangeDialog()).execute();
+                try {
+                    new ExchangeOperation(frame.getMoneyDisplayLabel(), frame.getExchangeDialog()).execute();
+                } catch (SQLException ex) {
+                }
             }
         });
     }
